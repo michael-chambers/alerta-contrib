7778679b2eb93a13edb08680a8e21930cc4541f1
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2023-02-16 14:36:35 -0800

    fixed attribute name reference

--- plugins/salesforce/alerta_salesforce.py
+++ plugins/salesforce/alerta_salesforce.py
@@ -144,7 +144,7 @@ class SFIntegration(PluginBase):
             configValues = read_sf_auth_values(alert.customer, alert.environment, alert.resource)
             self.client = SalesforceClient(configValues)
             LOG.debug("Preparing to send alert to SalesForce")
-            if not 'case_link' in alert.attributes.keys():
+            if not 'salesforce' in alert.attributes.keys():
                 sf_response = self.client.create_case(f'SRE [{alert.severity.upper()}] {alert.event}', alert.text, alert.serialize)
                 if sf_response['status'] == 'created':
                     case_link = "https://mirantis.my.salesforce.com/{}".format(sf_response['case_id'])
