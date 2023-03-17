378c8f5ea0aacc5932a95ee46acc45a6c368c852
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2023-03-17 16:48:21 -0700

    it works now

--- plugins/auto-blackout/alerta_autoblackout.py
+++ plugins/auto-blackout/alerta_autoblackout.py
@@ -4,25 +4,42 @@ import requests
 
 from alerta.plugins import PluginBase, app
 
+from alerta.models.alert import Alert
+
 # set plugin logger
 LOG = logging.getLogger('alerta.plugins.auto-blackout')
 
-API_URL = 'http://localhost:8080/api'
+BASE_URL = app.config.get('BASE_URL') or os.environ.get('BASE_URL')
+API_URL = f'http://localhost:8080{BASE_URL}'
 API_KEY = os.environ.get('ALERTA_API_KEY')
-UPDATE_EVENT = "MCCClusterUpdating"
+BLACKOUT_EVENTS = app.config.get('AUTOBLACKOUT_EVENTS')
 
 class AutoBlackout(PluginBase):
+    def __init__(self, name=None):
+        self.blackoutUrl=f'{API_URL}/blackout'
+        self.blackoutHeaders={
+            "Authorization": f"Key {API_KEY}",
+            "Content-type": "application/json"
+        }
+        super(AutoBlackout, self).__init__(name)
+
     def pre_receive(self, alert, **kwargs):
-        # TODO
         return alert
     
     def post_receive(self, alert, **kwargs):
-        if alert.event == "MCCClusterUpdating":
-            blackoutRequest = {
-                "environment": alert.environment,
-                "event": UPDATE_EVENT
-            }
-            requests.post(f'{API_URL}/blackout', data=blackoutRequest, headers=f'Authorization: Key {API_KEY}')
+        if alert is not None:
+            if len(BLACKOUT_EVENTS) == 0:
+                return alert
+            for event in BLACKOUT_EVENTS:
+                if str.upper(event) == str.upper(alert.event):
+                    LOG.debug(f'blackout event {alert.event} identified for alert {alert.id}')
+                    blackoutRequest = {
+                        "environment": alert.environment
+                    }
+                    try:
+                        requests.post(self.blackoutUrl, json=blackoutRequest, headers=self.blackoutHeaders)
+                    except Exception:
+                        LOG.error(f'Unable to complete POST API request to create blackout for alert {alert.id}')
         return alert
 
     def status_change(self, alert, status, text, **kwargs):
